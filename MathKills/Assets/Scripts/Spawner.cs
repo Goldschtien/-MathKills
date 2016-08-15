7c79154cf37298088a917598470eaa810d27d2b3
Cindy Pham <cindy619mysterio@yahoo.com>
2016-08-15 16:11:32 -0500

    Changes: Created game over animation, destroy function embedded in Spawner script

--- MathKills/Assets/Scripts/Spawner.cs
+++ MathKills/Assets/Scripts/Spawner.cs
@@ -1,11 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Spawner : MonoBehaviour {
 
 	// Numbers arroy
 	public GameObject player;
 	public GameObject[] numbers;
+	public GameObject instantiatedNumber;
+	List<GameObject> instantiatedNumbers = new List<GameObject>();
+	List<GameObject> numbersToRemove = new List<GameObject>();
+
+
 
 	public void spawnNext() {
 		// Choose a random index
@@ -20,14 +26,21 @@ public class Spawner : MonoBehaviour {
 
 
 		// Spawn number at Spawner's position (Spawner will always be above player by a constant distance)
-		Instantiate(numbers[i], new Vector3(spawnX, spawnY + player.transform.position.y, spawnZ), Quaternion.Euler(0, 0, Random.Range(0, 360)));
+		instantiatedNumber = (GameObject) Instantiate(numbers[i], new Vector3(spawnX, spawnY + player.transform.position.y, spawnZ), Quaternion.Euler(0, 0, Random.Range(0, 360)));
+		// Add the instantiated number to a list
+		instantiatedNumbers.Add(instantiatedNumber);
 	}
 
+	// Rescales all the number prefabs
 	public void resizeNumbers(){
-		// Loop through each number and rescale
 		foreach(GameObject number in numbers){
 			number.transform.localScale = new Vector3 (1.8f, 1.8f, 1.8f);
 		}
+	}
+
+	// Destroys an instantiated object
+	public void destroy(GameObject obj) {
+		Destroy (obj);
 	}
 
 	void Start() {
@@ -37,4 +50,20 @@ public class Spawner : MonoBehaviour {
 		InvokeRepeating("spawnNext", 0, 2);
 	}
 		
+	void Update() {
+		// Destroys instantiated numbers that are below the eraser
+		// or too far off screen
+		// TESTING - Currently destroys objects that are 4 lower than player
+		// Solutions: Must make background screen taller or have camera more zoomed in
+		foreach (GameObject number in instantiatedNumbers) {
+			if (number.transform.position.y + 5 <= player.transform.position.y) {
+				numbersToRemove.Add(number);
+			}
+		}
+
+		foreach (GameObject number in numbersToRemove) {
+			destroy(number);
+			instantiatedNumbers.Remove(number);
+		}
+	}
 }
